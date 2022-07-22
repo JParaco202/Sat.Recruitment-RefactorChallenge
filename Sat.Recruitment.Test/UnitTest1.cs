b63645aad562996adda911030e32ab5f82745786
Johann <75845193+JParaco202@users.noreply.github.com>
2022-07-22 14:59:50 -0300

    Update UnitTest1.cs

--- Sat.Recruitment.Test/UnitTest1.cs
+++ Sat.Recruitment.Test/UnitTest1.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Dynamic;
-
-using Microsoft.AspNetCore.Mvc;
-
 using Sat.Recruitment.Api.Controllers;
-
 using Xunit;
 
 namespace Sat.Recruitment.Test
@@ -16,11 +10,9 @@ namespace Sat.Recruitment.Test
         public void Test1()
         {
             var userController = new UsersController();
-
             var result = userController.CreateUser("Mike", "mike@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
 
-
-            Assert.Equal(true, result.IsSuccess);
+            Assert.True(result.IsSuccess);
             Assert.Equal("User Created", result.Errors);
         }
 
@@ -28,11 +20,9 @@ namespace Sat.Recruitment.Test
         public void Test2()
         {
             var userController = new UsersController();
-
             var result = userController.CreateUser("Agustina", "Agustina@gmail.com", "Av. Juan G", "+349 1122354215", "Normal", "124").Result;
 
-
-            Assert.Equal(false, result.IsSuccess);
+            Assert.False(result.IsSuccess);
             Assert.Equal("The user is duplicated", result.Errors);
         }
     }
